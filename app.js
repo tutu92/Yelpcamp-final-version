ed747750a58f7792f08b38205ef0e7ea6c6c6189
Tuomas Lampinen <tuu-mas@hotmail.com>
2018-01-10 12:48:20 +0000

    add mongolab

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ var commentRoutes       = require("./routes/comments"),
     indexRoutes         = require("./routes/index")
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/yelp_camp_v15", {useMongoClient: true});
+// mongoose.connect("mongodb://localhost/yelp_camp_v15", {useMongoClient: true});
+mongoose.connect("mongodb://Tuomas:password@ds247317.mlab.com:47317/yelpcampproject", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
