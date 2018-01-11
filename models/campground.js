7057df951e64acb005e75f0e1d4a07cdf07be4e9
Tuomas Lampinen <tuu-mas@hotmail.com>
2018-01-11 12:45:31 +0000

    add div with id map to show, style map, add location lat and lng to models

--- models/campground.js
+++ models/campground.js
@@ -7,6 +7,9 @@ var campgroundSchema = new mongoose.Schema({
    price: String,
    image: String,
    description: String,
+   location: String,
+   lat: Number,
+   lng: Number,
    createdAt: { type: Date, default: Date.now },
    author: {
       id: {
